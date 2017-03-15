a940b3586eb8b5fd636b889e04e61c54edfb4f44
joaofcosta <dinojoaocosta@gmail.com>
2017-03-15 16:19:39 +0000

    Add temporary skull_extract.

--- skull_extract.py
+++ skull_extract.py
@@ -1,6 +1,6 @@
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Dense, Dropout, Activation, Flatten, ZeroPadding3D
 from keras.layers import Convolution2D, MaxPooling2D, Convolution3D, MaxPooling3D
 from keras.utils import np_utils
 from keras.datasets import mnist
@@ -23,55 +23,27 @@ print(yt.shape)
 print(xtt.shape)
 print(ytt.shape)
 
-# (X_train, y_train), (X_test, y_test) = mnist.load_data()
-
-X_train = np.rollaxis(xt.get_data().reshape((1,) + xt.shape + (1,)), 3, 1)
-X_test = np.rollaxis(xtt.get_data().reshape((1,) + xtt.shape + (1,)), 3, 1)
-
-Y_train = np.rollaxis(yt.get_data(), 2, 0)
-Y_test = np.rollaxis(ytt.get_data(), 2, 0)
-
-print(X_train.dtype)
-print(np.amax(X_train))
-print(np.amax(X_test))
-print("X_train: ", X_train)
-print("X_train[0]: ", X_train[0])
-print("X_train[0][0]: ", X_train[0][0])
-print("len X_train: ", len(X_train))
-print("len X_train[0]: ", len(X_train[0]))
-print("len X_train[0][0]: ", len(X_train[0][0]))
-
-
-print("Y_train: ", Y_train)
-print("Y_train[0]: ", Y_train[0])
-print("Y_train[0][0]: ", Y_train[0][0])
-print("len Y_train: ", len(Y_train))
-print("len Y_train[0]: ", len(Y_train[0]))
-print("len Y_train[0][0]: ", len(Y_train[0][0]))
-
+X_train = np.rollaxis(xt.get_data(), 2).reshape(200, 44176)
+Y_train = np.rollaxis(yt.get_data(), 2).reshape(200, 44176)
+ 
 model = Sequential()
-# First layer. We need to tell it the input_shape(rows,cols,channels)
-model.add(Convolution3D(32,3,3,3, activation="relu", input_shape=(200, 176, 251, 1)))
-# Second layer
-model.add(Convolution3D(32, 3, 3, 3, activation='relu'))
-# Third layer - MaxPooling. Size decreases here
-model.add(MaxPooling3D(pool_size=(2,2,2)))
-# Dropout is a technique to reduce overfitting probabilty
-model.add(Dropout(0.25))
-# Weights need to be flattened before the fully connected layer (dense)
-model.add(Flatten())
-# Fully Connected layer
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-# Output layer
-model.add(Dense(10, activation='linear'))
-
+model.add(Dense(5000, input_shape=(44176,)))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(Dense(10000))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(Dense(44176))
+# model.add(ZeroPadding3D(padding=(1, 1, 1), dim_ordering='default', input_shape=(200, 176, 251, 1)))
+# 
+# model.add(Convolution3D(1, 3, 3, 3, activation="relu" ))
+ 
 # Declare the loss function and the optimizer
 model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
-
+ 
 # Train model
-model.fit(X_train, Y_train, batch_size=32, nb_epoch=10, verbose=1)
-
-# Evaluate model
+model.fit(X_train, Y_train, batch_size=3, nb_epoch=1, verbose=1)
+# 
+# # Evaluate model
 score = model.evaluate(X_test, Y_test, verbose=0)
 print(score)
