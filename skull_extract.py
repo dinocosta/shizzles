e628d7bd3ac6abb6f6d4a23fd37e3ba0b33b9063
FranciscoRibeiro <kikoribeiro95@gmail.com>
2017-05-03 14:39:20 +0100

    Add .csv logging to skull_extract

--- skull_extract.py
+++ skull_extract.py
@@ -6,7 +6,8 @@ from keras.utils import np_utils
 from keras.datasets import mnist
 from matplotlib import pyplot as plt
 import time
-import normalization as norm
+import sys
+import psutil
 
 # NIFTI
 import os
@@ -17,23 +18,30 @@ mris = []
 masks = []
 # Load images
 # Splits mris from their masks by ordering and filtering odd from even
-files_list = sorted(os.listdir("/home/dl_skull/normalized_images"))
-for i in range(len(files_list)):
-    filepath = "/home/dl_skull/normalized_images/" + files_list[i]
+mris_list = sorted(os.listdir("/mnt/disk3/datasets_rm/data_set_skull/dl_skull_trab/mris"))
+masks_list = sorted(os.listdir("/mnt/disk3/datasets_rm/data_set_skull/dl_skull_trab/masks"))
+
+print("Loading MRI's...")
+#for i in range(len(mris_list)):
+for i in range(2):
+    filepath = "/mnt/disk3/datasets_rm/data_set_skull/dl_skull_trab/mris/" + mris_list[i]
     file = nib.load(filepath).get_data()
-    # Masks are named _bet_mask so they are ordered first
-    if ('mask' in filepath):
-        masks.append(file)
-    else:
-        mris.append(file)
+    mris.append(file)
+
+print("Loading masks...")
+#for i in range(len(masks_list)):
+for i in range(2):
+    filepath = "/mnt/disk3/datasets_rm/data_set_skull/dl_skull_trab/masks/" + masks_list[i]
+    file = nib.load(filepath).get_data()
+    masks.append(file)
 
 # Concatenate exams on the samples axis
 mris = np.concatenate(mris, 2)
 masks = np.concatenate(masks, 2)
 
 # Order the dimensions to have (samples, rows, cols, channels)
-mris = np.rollaxis(norm.normalize_image(mris), 2).reshape(mris.shape[2], 176, 256, 1)
-masks = np.rollaxis(norm.normalize_image(masks), 2).reshape(masks.shape[2], 176, 256, 1)
+mris = np.rollaxis(mris, 2).reshape(mris.shape[2], 176, 256, 1)
+masks = np.rollaxis(masks, 2).reshape(masks.shape[2], 176, 256, 1)
 
 model = Sequential()
 model.add(ZeroPadding2D(padding=(2, 2), input_shape=(176, 256, 1)))
@@ -50,12 +58,19 @@ model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
 # Train model
 history = model.fit(mris, masks, batch_size=3, nb_epoch=1, verbose=1, validation_split=0.2)
 
+p = psutil.Process()
+cpu_time = p.cpu_times()[0]
+mem = p.memory_info()[0]
+
 # Save Model
 timestamp = time.strftime("%Y%m%d-%H%M%S")
 model.save("logs/"+ timestamp + ".h5")
 
 # Log results
-#with open("logs/log.txt", "a") as myfile:
-#        myfile.write(timestamp)
-#        myfile.write("Loss:" + history.history['loss'][-1])
-#        myfile.write("Acc:" + history.history['accuracy'][-1])
+with open("logs/log.csv", "a") as myfile:
+    myfile.write(",")
+    myfile.write("\"" + ascii(model).strip() + "\",")
+    myfile.write("{},".format(history.history['loss'][-1]))
+    myfile.write("{},".format(history.history['acc'][-1]))
+    myfile.write("{},".format(mem))
+    myfile.write("{},\n".format(cpu_time))
