f0b8ec0015a4d1f375c9329578f6c2b482226571
FranciscoRibeiro <kikoribeiro95@gmail.com>
2017-06-17 13:07:25 +0100

    Update training script to use dice coefficient and some minor fixes. Add main to skull_extract so it can be imported by application without being run automatically

--- application.py
+++ application.py
@@ -1,11 +1,10 @@
 import os
 import numpy as np 
 from keras.models import load_model
-import normalization as norm
 import nibabel as nib
+from skull_extract import dice_coef_loss
 
-trained_models = os.listdir("logs")
-trained_models.remove("log.txt")
+trained_models = sorted(os.listdir("logs"))
 for i in range(len(trained_models)):
     print(str(i) + " - " + trained_models[i])
 
@@ -14,14 +13,17 @@ try:
 except ValueError:
     print("Not a number")
 
-model = load_model("logs/" + trained_models[model_number])
+model = load_model("logs/" + trained_models[model_number], custom_objects={'dice_coef_loss': dice_coef_loss})
 
-mri = nib.load("/home/dl_skull/normalized_images/SW3944C_str_crop_norm.nii.gz")
+mri = nib.load("/mnt/disk3/datasets_rm/data_set_skull/dl_skull_trab/mris/101_str_crop.nii.gz")
 affine = mri.affine
 mri = mri.get_data()
 
-mri = np.rollaxis(norm.normalize_image(mri), 2).reshape(mri.shape[2], 176, 256, 1)
+mri = np.rollaxis(mri, 2).reshape(mri.shape[2], 176, 256, 1)
 prediction = model.predict(mri)
 prediction = np.rollaxis(prediction, 0, 3).reshape(176, 256, mri.shape[0])
+
 prediction_img = nib.Nifti1Image(prediction, affine)
-nib.save(prediction_img, "teste.nii")
+nib.save(prediction_img, "prediction_img.nii")
+generated_mask = nib.Nifti1Image(np.round(prediction), affine)
+nib.save(generated_mask, "generated_mask.nii")
--- skull_extract.py
+++ skull_extract.py
@@ -5,6 +5,7 @@ from keras.layers import Convolution2D, MaxPooling2D, Convolution3D, MaxPooling3
 from keras.utils import np_utils
 from keras.datasets import mnist
 from keras_diagram import ascii
+from keras import backend as K
 from matplotlib import pyplot as plt
 import time
 import sys
@@ -15,61 +16,72 @@ import os
 import nibabel as nib
 from nibabel.testing import data_path
 
-mris = []
-masks = []
-# Load images
-# Splits mris from their masks by ordering and filtering odd from even
-mris_list = sorted(os.listdir("/mnt/disk3/datasets_rm/data_set_skull/dl_skull_trab/mris"))
-masks_list = sorted(os.listdir("/mnt/disk3/datasets_rm/data_set_skull/dl_skull_trab/masks"))
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + 1) / (K.sum(y_true_f) + K.sum(y_pred_f) + 1)
 
-print("Loading MRI's...")
-for i in range(len(mris_list)):
-    filepath = "/mnt/disk3/datasets_rm/data_set_skull/dl_skull_trab/mris/" + mris_list[i]
-    file = nib.load(filepath).get_data()
-    mris.append(file)
+def dice_coef_loss(y_true, y_pred):
+    return -dice_coef(y_true, y_pred)
 
-print("Loading masks...")
-for i in range(len(masks_list)):
-    filepath = "/mnt/disk3/datasets_rm/data_set_skull/dl_skull_trab/masks/" + masks_list[i]
-    file = nib.load(filepath).get_data()
-    masks.append(file)
 
-# Concatenate exams on the samples axis
-mris = np.concatenate(mris, 2)
-masks = np.concatenate(masks, 2)
+if __name__ == "__main__":
+    mris = []
+    masks = []
+    # Load images
+    # Splits mris from their masks by ordering and filtering odd from even
+    mris_list = sorted(os.listdir("/mnt/disk3/datasets_rm/data_set_skull/dl_skull_trab/mris"))
+    masks_list = sorted(os.listdir("/mnt/disk3/datasets_rm/data_set_skull/dl_skull_trab/masks"))
 
-# Order the dimensions to have (samples, rows, cols, channels)
-mris = np.rollaxis(mris, 2).reshape(mris.shape[2], 176, 256, 1)
-masks = np.rollaxis(masks, 2).reshape(masks.shape[2], 176, 256, 1)
+    print("Loading MRI's...")
+    for i in range(len(mris_list)):
+        filepath = "/mnt/disk3/datasets_rm/data_set_skull/dl_skull_trab/mris/" + mris_list[i]
+        file = nib.load(filepath).get_data()
+        mris.append(file)
 
-model = Sequential()
-model.add(ZeroPadding2D(padding=(2, 2), input_shape=(176, 256, 1)))
-model.add(Convolution2D(32, 3, 3, activation="relu"))
-model.add(Dropout(0.2))
-model.add(Convolution2D(64, 3, 3, activation="relu"))
-model.add(Dropout(0.2))
-model.add(Dense(16))
-model.add(Dense(1))
- 
-# Declare the loss function and the optimizer
-model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
- 
-# Train model
-history = model.fit(mris, masks, batch_size=3, nb_epoch=10, verbose=1, validation_split=0.2)
+    print("Loading masks...")
+    for i in range(len(masks_list)):
+        filepath = "/mnt/disk3/datasets_rm/data_set_skull/dl_skull_trab/masks/" + masks_list[i]
+        file = nib.load(filepath).get_data()
+        masks.append(file)
 
-p = psutil.Process()
-cpu_time = p.cpu_times()[0]
-mem = p.memory_info()[0]
+    # Concatenate exams on the samples axis
+    mris = np.concatenate(mris, 2)
+    masks = np.concatenate(masks, 2)
 
-# Save Model
-timestamp = time.strftime("%Y%m%d-%H%M%S")
-model.save("logs/"+ timestamp + ".h5")
+    # Order the dimensions to have (samples, rows, cols, channels)
+    mris = np.rollaxis(mris, 2).reshape(mris.shape[2], 176, 256, 1)
+    masks = np.rollaxis(masks, 2).reshape(masks.shape[2], 176, 256, 1)
 
-# Log results
-with open("logs/log.csv", "a") as myfile:
-    myfile.write(",")
-    myfile.write("\"" + ascii(model).strip() + "\",")
-    myfile.write("{},".format(history.history['loss'][-1]))
-    myfile.write("{},".format(history.history['acc'][-1]))
-    myfile.write("{},".format(mem))
-    myfile.write("{},\n".format(cpu_time))
+    model = Sequential()
+    model.add(ZeroPadding2D(padding=(2, 2), input_shape=(176, 256, 1)))
+    model.add(Convolution2D(32, 3, 3, activation="relu"))
+    model.add(Dropout(0.2))
+    model.add(Convolution2D(64, 3, 3, activation="relu"))
+    model.add(Dropout(0.2))
+    model.add(Dense(16))
+    model.add(Dense(1))
+     
+    # Declare the loss function and the optimizer
+    model.compile(loss=dice_coef_loss, optimizer='adam', metrics=['accuracy'])
+     
+    # Train model
+    history = model.fit(mris, masks, batch_size=3, nb_epoch=20, verbose=1, validation_split=0.2)
+
+    p = psutil.Process()
+    cpu_time = p.cpu_times()[0]
+    mem = p.memory_info()[0]
+
+    # Save Model
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+    model.save("logs/"+ timestamp + ".h5")
+
+    # Log results
+    with open("logs/log.csv", "a") as myfile:
+        myfile.write(",")
+        myfile.write("\"" + ascii(model).strip() + "\",")
+        myfile.write("{},".format(history.history['loss'][-1]))
+        myfile.write("{},".format(history.history['dice_coef'][-1]))
+        myfile.write("{},".format(mem))
+        myfile.write("{},\n".format(cpu_time))
